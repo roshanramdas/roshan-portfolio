29d0378dd5be31b3748142f23aefe0a3e27f5186
Roshan <roshanramdas@gmail.com>
2024-02-21 15:18:31 -0500

    UI Change on itemCard

--- app/components/itemCard.tsx
+++ app/components/itemCard.tsx
@@ -12,7 +12,7 @@ interface ExperienceItemProps {
 const ExperienceItem: React.FC<ExperienceItemProps> = ({ title, company, location, date, responsibilities }) => (
   <div className="mb-6">
     <h3 className="text-xl font-bold mb-2">{title}</h3>
-    <p className="text-gray-500">{company} ({location}) | {date}</p>
+    <p className="text-gray-500">{company} {location} | {date}</p>
     <ul className="list-disc ml-6">
       {responsibilities.map((responsibility, index) => (
         <li key={index} className="mb-2">{responsibility}</li>
